d520a16a6bf3e1b7b02447cbc3e61f0954e94a7e
Valin Short <valin.short1998@gmail.com>
2018-05-02 16:21:53 -0400

    chapter 4 assesment

--- app/src/main/java/com/example/rodneytressler/week4assessment/Player.java
+++ app/src/main/java/com/example/rodneytressler/week4assessment/Player.java
@@ -1,9 +1,49 @@
 package com.example.rodneytressler.week4assessment;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by rodneytressler on 12/17/17.
  */
 
-public class Player {
+public class Player implements Parcelable {
 
+    private String PlayerName;
+    private String PlayerTeam;
+    private int PlayerNumber;
+
+    // Generated Parcleable //
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.PlayerName);
+        dest.writeString(this.PlayerTeam);
+        dest.writeInt(this.PlayerNumber);
+    }
+
+    public Player() {
+    }
+
+    protected Player(Parcel in) {
+        this.PlayerName = in.readString();
+        this.PlayerTeam = in.readString();
+        this.PlayerNumber = in.readInt();
+    }
+
+    public static final Parcelable.Creator<Player> CREATOR = new Parcelable.Creator<Player>() {
+        @Override
+        public Player createFromParcel(Parcel source) {
+            return new Player(source);
+        }
+
+        @Override
+        public Player[] newArray(int size) {
+            return new Player[size];
+        }
+    };
 }
